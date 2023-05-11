17fc1e9e0d9ea1a26e21eae44fc52c11b2959551
IT21006166 <it21006166@my.sliit.lk>
2023-05-11 19:44:18 +0530

    modified the layout

--- fg-client/src/Components/StatusBar/StatusUpdate.js
+++ fg-client/src/Components/StatusBar/StatusUpdate.js
@@ -7,8 +7,8 @@ import axios from "axios";
 import { useAuth } from "../Authentication/auth";
 import Cookies from "universal-cookie";
 import { Link, useParams, useNavigate } from "react-router-dom";
-import {  Dialog } from "@material-ui/core";
-
+import { Dialog } from "@material-ui/core";
+import { IoClose, IoCloseCircle, IoCloseCircleSharp } from "react-icons/io5";
 
 function StatusUpdate() {
   const cookies = new Cookies();
@@ -40,24 +40,26 @@ function StatusUpdate() {
     };
   }
 
-  const {id} = useParams();
+  const { id } = useParams();
   console.log(id);
 
   const getStatus = async () => {
     //getStatus is the function to get the data from the backend
-    await axios.get("http://localhost:8081/status/" + id , options).then((res) => {
-      setStatus(res.data);  
-      setStatus_id(res.data.status_id);
-      setUser_id(res.data.user_id);
-      setStatusPath(res.data.statusPath);
-      setCaption(res.data.caption);
-      setTimestamp(res.data.timestamp);
+    await axios
+      .get("http://localhost:8081/status/" + id, options)
+      .then((res) => {
+        setStatus(res.data);
+        setStatus_id(res.data.status_id);
+        setUser_id(res.data.user_id);
+        setStatusPath(res.data.statusPath);
+        setCaption(res.data.caption);
+        setTimestamp(res.data.timestamp);
 
-      console.log("Status: " + status.status_id);
-
-  }).catch((err) => {
-      alert(err);
-  })
+        console.log("Status: " + status.status_id);
+      })
+      .catch((err) => {
+        alert(err);
+      });
   };
 
   useEffect(() => {
@@ -76,66 +78,80 @@ function StatusUpdate() {
         alert(err.message);
       });
   };
-  
+
   const updateStatus = async (id) => {
-
-        const newStatus = {
-          // status_id:"12345",
-          user_id: { id: localStorage.getItem("user_id").toString() },
-          statusPath,
-          caption,
-          timestamp,
-        };
-        // console.log(newStatus);
-        await axios
-          .put("http://localhost:8081/status/" + id, newStatus, options)
-          .then(() => {
-             getStatus();
-            // setState(!state);
-            // setShow(false);
-            navigate("/home");
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-
+    const newStatus = {
+      // status_id:"12345",
+      user_id: { id: localStorage.getItem("user_id").toString() },
+      statusPath,
+      caption,
+      timestamp,
+    };
+    // console.log(newStatus);
+    await axios
+      .put("http://localhost:8081/status/" + id, newStatus, options)
+      .then(() => {
+        getStatus();
+        // setState(!state);
+        // setShow(false);
+        navigate("/home");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   const goBack = () => {
-    navigate('/home');
-  }
+    navigate("/home");
+  };
 
   return (
-    <div >
-      <Dialog open={true}
+    <div>
+      <Dialog
+      style={{backgroundColor:"bisque"}}
+        open={true}
         classname="upload_dialogbox"
         aria-labelledby="simple-dialog-title"
       >
-       <div className="upload_header">{user_id.username}</div>
-        <button onClick={() => goBack()}><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRQeV7P3xoHW_Yc4bNp0B5kXCOmuSbxdck1Q&usqp=CAU"
-        style={{width:"40px", height:"40px", position:"absolute", top:"20px", right:"40px"}}
-        /></button>
+        <div className="upload_header">{user_id.username}</div>
+        <button
+          style={{
+            fontSize: "40px",
+            color: "white",
+            position: "absolute",
+            top: "5px",
+            right: "5px",
+          }}
+          onClick={() => goBack()}
+        >
+          <IoCloseCircleSharp />
+        </button>
 
-        
         <img src={statusPath} className="upload_preview" />
 
-        <input
-          type="text"
-          className="upload_textbox"        
-          value = {caption} onChange={(e) => setCaption(e.target.value)}
+        <textarea
+          className="edit_textbox"
+          placeholder="Edit your caption..."
+          maxLength="100"
+          value={caption}
+          onChange={(e) => setCaption(e.target.value)}
         />
 
-        <div style={{display:"inline-block", textAlign:"center", margin:"10px"}}>
-        <input
-          type="button"
-          value="Edit"
-          className="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-4 ml-2 rounded-full"
-          onClick={() => updateStatus(id)}
-        />
+        <div style={{ backgroundColor: "black", textAlign: "center" }}>
+          <div
+            style={{ display: "inline-block", margin: "10px", padding: "10px" }}
+          >
+            <input
+              type="button"
+              value="Edit"
+              className="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-4 ml-2 rounded-full"
+              onClick={() => updateStatus(id)}
+            />
+          </div>
         </div>
       </Dialog>
     </div>
   );
-  }
+}
 
 export default StatusUpdate;
